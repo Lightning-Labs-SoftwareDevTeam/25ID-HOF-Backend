7d7725bb7fd1cd5fec5a16b9f9a64c7450e53bbb
Brian Savidge <bsav0016@gmail.com>
2024-11-15 16:12:21 -1000

    Changed timeout location

--- app.py
+++ app.py
@@ -18,14 +18,13 @@ app = Flask(__name__)
 
 CORS(app, origins="*", supports_credentials=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = env.DB_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = env.DB_URI + "?connect_timeout=60"
 app.config["SECRET_KEY"] = env.FLASK_SECRETKEY
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     "pool_size": 10,
     "max_overflow": 20,
     "pool_recycle": 3600,
-    "pool_pre_ping": True,
-    "connect_timeout": 60
+    "pool_pre_ping": True
 }
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 jwt = JWTManager(app)
