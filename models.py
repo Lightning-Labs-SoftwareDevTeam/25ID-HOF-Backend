3b39df45a0f5b7cbf7d8ff27eaa2edd4b18876fa
Brian Savidge <bsav0016@gmail.com>
2024-11-18 11:58:43 -1000

    Increased token length

--- models.py
+++ models.py
@@ -26,7 +26,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
-    token = db.Column(db.String(256), nullable=True)
+    token = db.Column(db.String(512), nullable=True)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
